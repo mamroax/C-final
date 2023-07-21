258da2840bd13ca0de5c5a5ae8f6b0baabf8e60d
mamroax <mamroax@gmail.com>
2023-07-21 13:15:29 +0300

    program ready

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿String[] myArray = {"Russia", "Denmark", "Kazan"};
+
+int number = 3;
+
+int myArrayLen = myArray.Length;
+String[] result = new String[myArrayLen];
+int counter = 0;
+
+for (int i = 0; i < myArray.Length; i++)    {
+    if (myArray[i].Length <= number){
+        result[counter] = myArray[i];
+        counter++;
+    }
+}
+
+for (int i = 0; i < result.Length; i++){
+    Console.Write(result[i]  + " ");
+}
+
